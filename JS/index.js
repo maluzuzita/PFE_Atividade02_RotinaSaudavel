65be66e1fb9541a177fb9b3ac376810774e097c3
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2026-03-26 15:53:31 -0300

    quarto commit

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,31 @@
 function adicionarHabito() {
-  const input = document.getElementById("inputHabito");
+  const inputHabito = document.getElementById("inputHabito");
+  const inputHorario = document.getElementById("inputHorario");
+  const inputData = document.getElementById("inputData");
   const tabela = document.getElementById("tabela");
 
-  if (input.value.trim() === "") {
+  if (inputHabito.value.trim() === "" || inputHorario.value === "" || inputData.value === "") {
     alert(
       "Poderia preencher o campo antes de adicionar? Adoraria saber qual o seu plano de hoje 😊",
+      inputHabito.value = "",
+      inputHorario.value = "",
+      inputData.value = "",
+      inputHabito.focus()
     );
   } else {
     const tr = document.createElement("tr");
+
     const tdHabito = document.createElement("td");
-    const valorHabito = input.value.trim();
+    const valorHabito = inputHabito.value.trim();
     tdHabito.innerText = valorHabito;
+
+    const tdHorario = document.createElement("td");
+    const valorHorario = inputHorario.value;
+    tdHorario.innerText = valorHorario;
+
+    const tdData = document.createElement("td");
+    const valorData = inputData.value;
+    tdData.innerText = valorData;
 
     const tdAcao = document.createElement("td");
     const btnRemover = document.createElement("button");
@@ -18,6 +33,7 @@ function adicionarHabito() {
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1v1H14a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+
       </svg> Remover
     `;
     btnRemover.className = "btn-remover";
@@ -34,22 +50,24 @@ function adicionarHabito() {
       </svg> Concluir
     `;
     btnConcluir.className = "btn-concluir";
-
     let clique = false;
     btnConcluir.onclick = function () {
       if ((clique = !clique)) {
         tdHabito.style.textDecoration = "line-through";
-        tdHabito.style.opacity = "0.7";
+        tdHabito.style.opacity = "0.5";
       } else {
         tdHabito.style.textDecoration = "none";
         tdHabito.style.opacity = "1";
       }
     };
 
+
     tdAcao.appendChild(btnRemover);
     tdAcao.appendChild(btnConcluir);
 
     tr.appendChild(tdHabito);
+    tr.appendChild(tdData);
+    tr.appendChild(tdHorario);
     tr.appendChild(tdAcao);
 
     tabela.appendChild(tr);
@@ -62,8 +80,10 @@ function adicionarHabito() {
       datalist.appendChild(novaOpcao);
     }
 
-    input.value = "";
-    input.focus();
+    inputHabito.value = "";
+    inputData.value = "";
+    inputHorario.value = "";
+    inputHabito.focus();
   }
 }
 
